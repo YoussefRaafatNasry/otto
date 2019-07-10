775681d244c5af9b893b01c17b6e69464a11a53f
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-07-10 15:13:31 +0200

    Ox31C
     * Add activate switch to layout.
     * Add Config class to store application configurations.
     * Initialize and bind IS_ACTIVE with the new switch.

---
+++ app/src/main/java/com/youssefraafatnasry/otto/Config.kt
@@ -0,0 +1,9 @@
+package com.youssefraafatnasry.otto
+
+class Config {
+
+    companion object {
+        var IS_ACTIVE = false
+    }
+
+}
--- app/src/main/java/com/youssefraafatnasry/otto/MainActivity.kt
+++ app/src/main/java/com/youssefraafatnasry/otto/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.youssefraafatnasry.otto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Application active status
+        Config.IS_ACTIVE = switch_activate.isChecked
+        switch_activate.setOnCheckedChangeListener { _, b -> Config.IS_ACTIVE = b }
+
     }
 }
