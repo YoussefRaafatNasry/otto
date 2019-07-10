616642f6e5844ae1d0c2a0f092b3090c87a18191
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-07-10 17:58:34 +0200

    Ox31F
     * Implement onNotificationPosted function.
     * Automatically reply to notification.
     * Choose reply dynamically based on saved rules.

--- app/src/main/java/com/youssefraafatnasry/otto/NotificationAutoReplier.kt
+++ app/src/main/java/com/youssefraafatnasry/otto/NotificationAutoReplier.kt
@@ -1,13 +1,61 @@
 package com.youssefraafatnasry.otto
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Bundle
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
 
 class NotificationAutoReplier : NotificationListenerService() {
 
-    override fun onNotificationPosted(sbn: StatusBarNotification?) {
-        super.onNotificationPosted(sbn)
-        throw NotImplementedError()
+    private val REPLY_KEYWORD = "reply"
+
+    inner class RepliableNotification(
+        var bundle: Bundle,
+        var pendingIntent: PendingIntent,
+        var remoteInputs: Array<RemoteInput>
+    )
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+
+        // Ignore ongoing notifications
+        if (!Config.IS_ACTIVE || sbn.isOngoing) return
+
+        // Get proper reply from saved rules
+        val text = sbn.notification.extras.get(NotificationCompat.EXTRA_TEXT).toString()
+        val reply: String
+        try {
+            reply = Config.RULES.first { text matches it.regex }.fullReply
+        } catch (e: NoSuchElementException) {
+            return
+        }
+
+        // A WearableExtender is used to bypass the "Sending reply.." toast
+        // when a normal StatusBarNotification is used to invoke actions
+        val wearableExtender = NotificationCompat.WearableExtender(sbn.notification)
+        val replyAction: NotificationCompat.Action
+        try {
+            replyAction = wearableExtender.actions.first {
+                it != null && it.remoteInputs != null && it.title.contains(REPLY_KEYWORD, true)
+            }
+        } catch (e: NoSuchElementException) {
+            return
+        }
+
+        // Reply to notification
+        val intent = Intent()
+        val n = RepliableNotification(
+            sbn.notification.extras,
+            replyAction.actionIntent,
+            replyAction.remoteInputs
+        )
+
+        n.remoteInputs.forEach { n.bundle.putCharSequence(it.resultKey, reply) }
+        RemoteInput.addResultsToIntent(n.remoteInputs, intent, n.bundle)
+        n.pendingIntent.send(applicationContext, 0, intent)
+
     }
 
 }
