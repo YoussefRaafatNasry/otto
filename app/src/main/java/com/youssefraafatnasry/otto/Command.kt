1b3d700b753150c4d31cced1286ea938bf309398
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-07-12 21:56:54 +0200

    Ox324
     * Remove NONE command.
     * Make Command parameter nullable in ReplyRule's constructor.
     * Replace invokeWith method with abstract method called execute,
       implemented in each command anonymous class.

--- app/src/main/java/com/youssefraafatnasry/otto/Command.kt
+++ app/src/main/java/com/youssefraafatnasry/otto/Command.kt
@@ -5,29 +5,20 @@ import android.media.ToneGenerator
 
 enum class Command {
 
-    NONE,
-    ADD_TRACK_TO_FAV,
-    NOTIFY_OWNER;
-
-    fun invokeWith(inputs: HashMap<String, String>) {
-
-        when (this) {
-
-            NONE -> { return }
-
-            ADD_TRACK_TO_FAV -> {
-                val trackId =
-                    SpotifyAPI.getTrackId(inputs[Template.TEXT].toString())
-                SpotifyAPI.likeTrack(trackId)
-            }
-
-            NOTIFY_OWNER -> {
-                ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME)
-                    .startTone(ToneGenerator.TONE_CDMA_PIP, 1000)
-            }
-
+    ADD_TRACK_TO_FAV {
+        override fun execute(inputs: HashMap<String, String>) {
+            val trackId = SpotifyAPI.getTrackId(inputs[Template.TEXT].toString())
+            SpotifyAPI.likeTrack(trackId)
         }
+    },
 
-    }
+    NOTIFY_OWNER {
+        override fun execute(inputs: HashMap<String, String>) {
+            ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME)
+                .startTone(ToneGenerator.TONE_CDMA_PIP, 1000)
+        }
+    };
+
+    abstract fun execute(inputs: HashMap<String, String>)
 
 }
--- app/src/main/java/com/youssefraafatnasry/otto/ReplyRule.kt
+++ app/src/main/java/com/youssefraafatnasry/otto/ReplyRule.kt
@@ -4,13 +4,13 @@ class ReplyRule(
     pattern: String,
     private val reply: String,
     private val exclude: String = "",
-    private val command: Command = Command.NONE
+    private val command: Command? = null
 ) {
 
     val regex = Regex(pattern, RegexOption.IGNORE_CASE)
 
     fun processReply(inputs: HashMap<String, String>): String {
-        command.invokeWith(inputs)
+        command?.execute(inputs)
         inputs[exclude] = ""
         return Config.PREFIX + Template.replace(reply, inputs) + Config.POSTFIX
     }
